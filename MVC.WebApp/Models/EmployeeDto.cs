b5eed3342855eb4867ec92b9010e8d2d8d5bca78
Imran <imran_gcs@hotmail.com>
2024-08-15 12:33:37 +0500

    Employee DTOs

--- MVC.WebApp/Models/EmployeeDto.cs
+++ MVC.WebApp/Models/EmployeeDto.cs
@@ -2,7 +2,7 @@
 
 namespace MVC.WebApp.Models
 {
-    public class EmployeeDto
+    public class EmployeeUpsertDto
     {
         public string? LastName { get; set; }
         public string? FirstName { get; set; }
@@ -21,8 +21,23 @@ namespace MVC.WebApp.Models
         public string? PhotoPath { get; set; }
     }
 
-    public class EmployeeUpsertDto: EmployeeDto
+    public class EmployeeDto
     {
         public int EmployeeID { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? Title { get; set; }
+        public string? TitleOfCourtesy { get; set; }
+        public string? BirthDate { get; set; }
+        public string? HireDate { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? HomePhone { get; set; }
+        public string? Extension { get; set; }
+        public int? ReportsTo { get; set; }
+        public string? PhotoPath { get; set; }
     }
 }
